c7f31b595aaa408e45175218095de618bb7d7b57
alanpnguyen123 <97857901+alanpnguyen123@users.noreply.github.com>
2023-01-30 16:52:06 -0500

    Making Changes to challenge page, card added

--- Mashed/components/Challenge.js
+++ Mashed/components/Challenge.js
@@ -1,7 +1,8 @@
 // challenge screen
-
+import { Card } from '@rneui/themed';
 import React, { Component } from "react";
 import { Button, View, Text } from "react-native";
+import { ListItem } from '@rneui/themed';
 
 export default function Challenge() {
   return (
@@ -9,4 +10,32 @@ export default function Challenge() {
       <Text>Challenge</Text>
     </View>
   );
+  <Card>
+          <Card.Image
+            style={{ padding: 0 }}
+            source={{
+              uri:
+                'https://dinnerthendessert.com/wp-content/uploads/2021/05/Berry-Stuffed-French-Toast-1-1-1.jpg'
+            }}
+          />
+          <Card.Title>Berry Stuffed French Toast</Card.Title>
+          <ListItem>
+            <ListItem.Content>
+              <Icon name="nutfree" type="material-community" color="purple"></Icon>
+              <ListItem.Title>Nut-Free</ListItem.Title>  
+            </ListItem.Content>  
+          </ListItem>    
+          <ListItem>
+            <ListItem.Content>
+            <Icon name="dietary" type="material-community" color="purple"></Icon>
+             <ListItem.Title>Halal, Kosher</ListItem.Title> 
+            </ListItem.Content>
+          </ListItem>
+          <ListItem>
+            <ListItem.Content>
+            <Icon name="WarningOutlined" type="material-community" color="purple"></Icon>
+             <ListItem.Title>Contains Eggs, Diary</ListItem.Title> 
+            </ListItem.Content>
+          </ListItem>
+        </Card>
 }
