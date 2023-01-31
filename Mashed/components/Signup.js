ecafaccc12a296bdacf1c5f90a8220fec7756e54
naomy <92695534+nnoguez@users.noreply.github.com>
2023-01-31 16:13:22 -0500

    c

--- Mashed/components/Signup.js
+++ Mashed/components/Signup.js
@@ -7,7 +7,7 @@ import { Button, View, Text } from "react-native";
 export default function Signup() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Sign up</Text>
+      <Text>Sign up!</Text>
     </View>
   );
 }
