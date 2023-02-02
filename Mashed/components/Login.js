78419510f29a08ff77273a0aaad42631b59c7f26
Shelbsconstant <shelbsconstant@gmail.com>
2023-02-02 10:29:37 -0500

    login edit

--- Mashed/components/Login.js
+++ Mashed/components/Login.js
@@ -7,9 +7,9 @@ import { SafeAreaView, View, ScrollView, Text, StyleSheet, SectionList, Touchabl
 import { SocialIcon, SocialIconProps, Input, Button, Icon } from '@rneui/themed'; 
 
 //no clue what this does yet lol
-/*type IconData ={
+/*type IconData = {
   type: SocialMediaType;
-  iconType:string
+  iconType:string;
 };
 
 //I think this is how I add the icon array (don't know how to input them into the login screen)
@@ -28,41 +28,62 @@ const dataList: Partial<IconData>[] = [
 
 type SocialIconsComponentProps = {};
 
-const SocialIcons:
-React.FunctionComponent<SocialIconsComponentProps*/
+const SocialIcons:React.FunctionComponent<SocialIconsComponentProps> = () => {
+  const socialProps = {};*/
+
 const styles = StyleSheet.create({
-  viewOne: {
+  backgroundPurple: {
+      zIndex:0,
       backgroundColor:'#9492ef',
       margin:0,
       flex: 1, 
       alignItems: "center", 
       justifyContent: "center",      
   },
-  viewTwo:{
-
-  },
-  viewThree:{
+  containerWhite:{
     zIndex:1,
-    position:'absolute',
-    flexDirection: "column",
+    position:'absolute',    
     bottom: 0,
-    height:"70%",
+    height:"60%",
     width: "100%",
-    borderTopLeftRadius: 50,
-    borderTopRightRadius: 50,
-    backgroundColor: '#fff',
-    
+    borderTopLeftRadius: 75,
+    borderTopRightRadius: 75,
+    backgroundColor: '#fff',    
     alignItems: "center", 
     justifyContent: "center",  
+  },
+  userInfo: {
+    marginTop:30,
+    zIndex:2,   
+    top: 0,
+    width: '75%',
+    alignItems: 'center',
+    //justifyContent: 'center',
+    //height:'100%',
+    padding: 10,
+   
+  },
+  userInputStyle: { 
+    zIndex:3,     
+    borderColor: 'gray',
+    borderWidth: 2,
+    borderRadius: 25,
+    padding:4,
+    textAlign:'center',
+    
+    
+  },  
+  buttonOne:{
+    zIndex:4,
+    backgroundColor: 'white',
+    color:'white',
+    textColor: 'black', 
+    fontWeight:'bold'
+  },
+  signUp:{
+    marginBottom:10,
+    zIndex:5,
 
-  },
-  imageStyle: {
-      zIndex:2,
-      width:"35%", 
-      height:25,    
-  },
-  input:{
-    borderRadius: 15
   }
 })
 
@@ -71,34 +92,36 @@ const styles = StyleSheet.create({
 export default function Login({navigation}) {
   return (  
 
-      <View style={styles.viewOne}>
-        <Image 
-          style={styles.imageStyle}
-          source={{uri:'https://i.postimg.cc/65XBkHNg/logo.png',}}/>
-    
-        <View style={styles.viewThree}>
-        <Input placeholder="Email Address"/>
-        <Input placeholder="Password"/>
-        <TouchableOpacity onPress={()=> navigation.navigate('Home')}>
-          <Button
-                title="LOG IN"
-                buttonStyle={{
-                  backgroundColor: '#9492ef',
-                  borderWidth: 2,
-                  borderColor: 'white',
-                  borderRadius: 30,
-                }}
-                containerStyle={{
-                  width: 200,
-                  marginHorizontal: 50,
-                  marginVertical: 10,
-                }}
-                titleStyle={{ fontWeight: 'bold' }}/>
-        </TouchableOpacity>
-        <Text>- Log In With -</Text>
-      </View>
-  
-  </View>
- 
+    <View style={styles.backgroundPurple}>       
+      <View style={styles.containerWhite}>
+        <View style={[styles.userInfo, {flex:1, flexDirection: 'column'},]}>   
+
+          <View style={{ width:'100%', height: 60}}><Input style={styles.userInputStyle} placeholder="Email Address"/></View>
+          <View style={{width:'100%', height:60}}><Input style={styles.userInputStyle} placeholder="Password"/></View>
+            
+          <View style={{ width:'100%'}}>
+            <TouchableOpacity onPress={()=> navigation.navigate('Home.js')}>
+              <Button
+                    title="LOG IN"
+                    buttonStyle={{backgroundColor: '#9492ef', borderWidth: 2, borderColor: 'white', borderRadius: 30,}}
+                    containerStyle={{ width: 200, marginHorizontal: 50,  marginVertical: 10,}}
+                    titleStyle={{ fontWeight: 'bold' }}>
+              </Button>
+            </TouchableOpacity>
+          </View>
+
+          <View style={{alignItems:'center',marginVertical: 20, width:'100%'}}>
+            <Text>- Log In With -</Text>
+            
+          </View>
+          
+          <View style={{alignItems:'center', width:'100%',marginTop:100}}>
+            <Text>Dont Have An Account? </Text>
+            <Button title="SIGN UP" type="clear" />
+          </View>
+
+        </View>     
+      </View> 
+    </View> 
   );
 }
