e78d0aa04e533ea8e36bcb5d36e5bafdb810ea86
alanpnguyen123 <97857901+alanpnguyen123@users.noreply.github.com>
2023-02-02 15:59:44 -0500

    Recipe page

--- Mashed/components/Recipe.js
+++ Mashed/components/Recipe.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, ScrollView, Text, StyleSheet, SectionList, TouchableOpacity, Image } from 'react-native';
-import { Avatar, Image, Badge, Card, CheckBox, ListItem } from '@rneui/themed';
+import { Avatar, Image, Badge, Card, CheckBox, ListItem, AirbnbRating } from '@rneui/themed';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function Recipe({ navigation }) {
@@ -27,48 +27,203 @@ export default function Recipe({ navigation }) {
             /> 
         </View>
         </View>
+        <ScrollView>
+        {/* image with heart icon */}
         <Image style={{width:"100%", height:300, marginTop: 5, marginTop: 5, borderBottomRightRadius: 25, borderBottomLeftRadius: 25}}
             source={{
             uri:'https://www.thespruceeats.com/thmb/a8cS7kg5bbsuFsJN-5zO3eOVvBE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/chicken-katsu-4778466-10-67e6122e936b418ab1a92f176709d299.jpg',
             }}>
-            
         </Image>
-        <ListItem>
-        <ListItem.Content>
-            <CheckBox
-                center
-                title="Click Here"
-                checked={check1}
-                onPress={() => setCheck1(!check1)}
-                />
-            <ListItem.Title>ingredient</ListItem.Title>
-        </ListItem.Content>
-            <CheckBox
-                center
-                title="Click Here"
-                checkedIcon="dot-circle-o"
-                uncheckedIcon="circle-o"
-                checked={check2}
-                onPress={() => setCheck2(!check2)}
-                />
 
-            <CheckBox
-                center
-                title={`Click Here to ${check3 ? 'Remove' : 'Add'} This Item`}
-                iconRight
-                iconType="material"
-                checkedIcon="clear"
-                uncheckedIcon="add"
-                checkedColor="red"
-                checked={check3}
-                onPress={() => setCheck3(!check3)}
-                />
+        {/* Title and star rating */}
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
+            <Text style={styles.header}>Cripsy Chicken Katsu</Text>
+            <AirbnbRating />
+        </View>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
+            <Text style={styles.Subheader}>By: sakuraiiko</Text>
+            <Text style={styles.Subheader}>618 Reviews</Text>
+        </View>
+        <View style={{ flexDirection: 'row',justifyContent: 'space-between', marginTop: 10 }}>
+            <Text style={styles.body}>Chicken katsu is Japanese-style fried chicken. 
+                                      This recipe can also be used to make tonkatsu by using pork cutlets instead of chicken. 
+                                      Serve with white rice and tonkatsu sauce.
+            </Text>
+            <Text>Total Time:25 mins 
+                  Prep: 15
+                  Cook:10
+                  Yield 4-6 Servings
+                  Level:Easy
+            </Text>
+        </View>
 
-        
-                
-    
-        
-			</ListItem>
+        <Text style={styles.header}>Dietary Restrictions</Text>
+        {/* Row 1 for dietary restrictions */}
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10, marginLeft: 3, marginRight: 50 }}>
+            <Text style={styles.Subheader}>
+              <Icon name="plus" color="#9492EF" size={20} paddingRight={20} />
+              Nut-free
+            </Text>
+            <Text style={styles.Subheader}>
+              <Icon name="plus" color="#9492EF" size={20} paddingRight={20} />
+              Halal, Kosher
+            </Text>
+            <Text style={styles.Subheader}>
+              <Icon name="plus" color="#9492EF" size={20} paddingRight={20} />
+              Lactose-Free
+            </Text>
+          </View>
+          {/* Row 2 for dietary restrictions */}
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10, marginLeft: 3, marginRight: 50 }}> 
+            <Text style={styles.fontBig}>
+              <Icon name="plus" color="#9492EF" size={20} />
+              Contains Egg, Dairy
+            </Text>
+          </View>
+            {/* Ingredients list drawer */}
+          <ListItem.Accordion
+                content={
+                    <>
+                    <Icon name="Place" size={30} />
+                    <ListItem.Content>
+                        <ListItem.Title>Ingredients</ListItem.Title>
+                    </ListItem.Content>
+                    </>
+                }
+                isExpanded={expanded}
+                onPress={() => {
+                    setExpanded(!expanded);
+                }}
+            >
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10, marginLeft: 3 }}>
+                    <ListItem.Content>
+                        <CheckBox
+                            center
+                            title="4 Skinless Boneless Chicken Breast"
+                            checked={check1}
+                            onPress={() => setCheck1(!check1)}
+                            />
+                    </ListItem.Content>
+                        <CheckBox
+                            center
+                            title="1 Cup of Panko Bread Crumbs"
+                            checked={check2}
+                            onPress={() => setCheck2(!check2)}
+                            />
+                </View>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10, marginLeft: 3}}>
+                    <ListItem.Content>
+                        <CheckBox
+                            center
+                            title="1 Egg, Beaten"
+                            checked={check1}
+                            onPress={() => setCheck1(!check1)}
+                            />
+                    </ListItem.Content>
+                        <CheckBox
+                            center
+                            title="2 Tablespoons all-purpose flour"
+                            checked={check2}
+                            onPress={() => setCheck2(!check2)}
+                            />
+                </View>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10, marginLeft: 3 }}>
+                    <ListItem.Content>
+                        <CheckBox
+                            center
+                            title="Salt and Pepper"
+                            checked={check1}
+                            onPress={() => setCheck1(!check1)}
+                            />
+                    </ListItem.Content>
+                        <CheckBox
+                            center
+                            title="1 Cup of vegetable oil"
+                            checked={check2}
+                            onPress={() => setCheck2(!check2)}
+                            />
+                </View>
+                <Button
+                    title="Edit Profile"
+                    buttonStyle={{
+                    backgroundColor: '#FFC42D',
+                    borderRadius: 30,
+                    padding: 1,
+                    }}
+                    containerStyle={{
+                        marginHorizontal: 25,
+                        marginVertical: 5,
+                    }}>
+                </Button>
+			</ListItem.Accordion>
+            {/* Directions and Images */}
+            <Text style={styles.header}>Directions</Text>
+            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10, marginLeft: 3,  }}>                
+                <ListItem>
+                    <ListItem.Content>
+                        <ListItem.Title style={styles.body}>1.Season chicken breasts on both sides with salt and pepper.</ListItem.Title>
+                    </ListItem.Content>
+                    <ListItem.Content>
+                        <ListItem.Title style={styles.body}>2.Place flour, beaten egg, and panko crumbs into separate shallow dishes.</ListItem.Title>
+                    </ListItem.Content>
+                    <ListItem.Content>
+                        <ListItem.Title style={styles.body}>3.Coat chicken breasts in flour, shaking off any excess; dip into egg, and then press into panko crumbs until well coated on both sides.</ListItem.Title>
+                    </ListItem.Content>
+                    <ListItem.Content>
+                        <ListItem.Title style={styles.body}>4.Heat oil in a large skillet over medium-high heat.</ListItem.Title>
+                    </ListItem.Content>
+                    <ListItem.Content>
+                        <ListItem.Title style={styles.body}>5.Place chicken in the hot oil, and fry until golden brown, 3 or 4 minutes per side.</ListItem.Title>
+                    </ListItem.Content>
+                    <ListItem.Content>
+                        <ListItem.Title style={styles.body}>6.Transfer to a paper towel-lined plate to drain.</ListItem.Title>
+                    </ListItem.Content>
+                    <ListItem.Content>
+                        <ListItem.Title style={styles.body}>7.Enjoy!</ListItem.Title>
+                    </ListItem.Content>
+                </ListItem>
+                <View >
+                <Image
+                    style={{
+                        width:"35%", 
+                        height:25}}
+                    source={{
+                    uri:'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQQgF1STU0UydNMATSPKJRhVk2wfqO3_cZQYzRnovQL2H9FoN5V',
+                    }}
+                 />
+                 <Image
+                    style={{
+                        width:"35%", 
+                        height:25}}
+                    source={{
+                    uri:'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTAy9m8-qjTNmf_rUUvxwZznaJv6mabXJb1hXdJ5mQH_1ekG9Hf',
+                    }}
+                 />
+                  <Image
+                    style={{
+                        width:"35%", 
+                        height:25}}
+                    source={{
+                    uri:'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTAy9m8-qjTNmf_rUUvxwZznaJv6mabXJb1hXdJ5mQH_1ekG9Hf',
+                    }}
+                 />
+                  <Image
+                    style={{
+                        width:"35%", 
+                        height:25}}
+                    source={{
+                    uri:'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTAy9m8-qjTNmf_rUUvxwZznaJv6mabXJb1hXdJ5mQH_1ekG9Hf',
+                    }}
+                 />
+                 </View>
+            </View>
+                  
+                    
+
+
+
+
+        </ScrollView>
         </>
     )
 }
@@ -78,5 +233,17 @@ const styles = StyleSheet.create({
         textAlign: "center",
         margin: 20,
         marginBottom: 10
+    },
+    header:{
+        fontSize: 20,
+        fontWeight: 'bold',
+    },
+    Subheader: {
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    body:{
+        fontSize: 15,
+        color: 'grey'
     }
 })
