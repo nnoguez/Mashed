084399f4c408bb9d0291adc3497d08a0a618aba4
Shelbsconstant <shelbsconstant@gmail.com>
2023-02-02 23:58:32 -0500

    Post edit

--- Mashed/components/Post.js
+++ Mashed/components/Post.js
@@ -8,7 +8,8 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 export default function Post ( {navigation} ) {
   return (
     <>
-    <View style={styles.viewOne}>
+  <View style={styles.viewOne}>
+
        <View style={{justifyContent: 'space-evenly', borderBottomWidth: 1, borderBottomColor: 'lightgrey', marginBottom: 30}}>
           <TouchableOpacity onPress={()=> navigation.navigate('Chat')}>
             <Icon style={{ marginLeft: 355, marginTop: 20 }} name="commenting" size={25} color="#FFC42D"/>
@@ -38,33 +39,61 @@ export default function Post ( {navigation} ) {
             </View>
 
             <View style={{flex: 2, justifyContent: 'space-evenly'}}>
-              <View style={[{flexDirection: 'row'}]}>
-                <Text style={{fontWeight: 'bold', fontSize: 20}}> @dnovatnak007 </Text>
-              </View>
-                <Button buttonStyle={{
-                  borderRadius: 25,
-                  width: 90,
-                  height: 30,
-                  backgroundColor: '#9492EF', 
-                }}
-                titleStyle={{
-                  fontSize: 10
-                }}>
+                <View style={[{flexDirection: 'row'}]}>
+                  <Text style={{fontWeight: 'bold', fontSize: 20}}> @dnovatnak007 </Text>
+                </View>
+                <Button 
+                    buttonStyle={{
+                      borderRadius: 25,
+                      width: 90,
+                      height: 30,
+                      backgroundColor: '#9492EF', 
+                    }}
+                    titleStyle={{
+                      fontSize: 10
+                    }}>
                   To Everyone v
                 </Button>
                 <Text margin> Currently in Europe with my girlfriend again and we visited this nice little cafe nea | </Text>
               </View>
-            </View>
-
-            <View style={styles.containerWhite}>
-        <View style={[styles.userInfo, {flex:1, flexDirection: 'column'},]}>   
-
+    </View>        
+  </View>
+      
+      <View style={styles.containerWhite}>
+          <View style={{flexDirection: 'row'}}>
+            <Image style={{ height:75,width:75, borderRadius: 5, margin: 5}}
+                  source={{
+                  uri:'https://kathrynskitchenblog.com/wp-content/uploads/2022/04/Shrimp-Ceviche-13-768x1152.jpg',
+                  }}
+              />
+            <Image style={{ height:75,width:75, borderRadius: 5, margin:5}}
+                  source={{
+                  uri:'https://kathrynskitchenblog.com/wp-content/uploads/2022/04/Shrimp-Ceviche-13-768x1152.jpg',
+                  }}
+              />
+              <Image style={{ height:75,width:75, borderRadius: 5, margin: 5}}
+                  source={{
+                  uri:'https://kathrynskitchenblog.com/wp-content/uploads/2022/04/Shrimp-Ceviche-13-768x1152.jpg',
+                  }}
+              />
+              <Image style={{ height:75,width:75, borderRadius: 5, margin: 5}}
+                  source={{
+                  uri:'https://kathrynskitchenblog.com/wp-content/uploads/2022/04/Shrimp-Ceviche-13-768x1152.jpg',
+                  }}
+              />
+          </View>
+        <View>
+          <Image style={{ height:'100%',width:420, borderRadius: 5, margin: 5}}
+                  source={{
+                  uri:'https://ibb.co/Fw2XVkR',
+                  }}/>
         </View>
+         
       </View>
 
 
 
-    </View>
+   
     </>
   );
 }
@@ -73,8 +102,26 @@ const styles = StyleSheet.create({
     viewOne: {
         textAlign: "center",
         margin: 20,
-        marginBottom: 10
-    },  
+        marginBottom: 175,
+       
+    }, 
+    containerWhite:{
+      zIndex:2,
+      //flex:1,
+      //position:'absolute',    
+      
+      height:"60%",
+      width: "100%",
+      borderTopLeftRadius: 45,
+      borderTopRightRadius:45,
+      shadowColor: 'gray',
+      shadowOpacity: '20%',  
+      shadowOffset:{width: 0, height: -3},
+      backgroundColor: '#fff',    
+      alignItems: "center",
+      paddingTop:20, 
+      //justifyContent: "center",  
+    }, 
     // userInfo: {
     //   marginTop:50,
     //   zIndex:2,   
