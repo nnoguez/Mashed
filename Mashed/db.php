83dd8ccbde7db8fd087655d1d76910ff5f42c371
naomy <92695534+nnoguez@users.noreply.github.com>
2023-03-08 23:29:06 -0500

    updated db.php

--- Mashed/db.php
+++ Mashed/db.php
@@ -5,7 +5,7 @@ $userName= "na404266";
 $password = "LeticiaPerk77!";
 $dbName = "na404266";
 
-$conn = mysqli_connect($serverName, $userName, $password, $dbName);
+$conn = mysqli_connect($serverName, $userName, $password, $dbName, $RecipeName, $RecipeStep, $RecipeIngredients);
 
 
 $encodedData = file_get_contents('php://input');  // take data from react native fetch API
