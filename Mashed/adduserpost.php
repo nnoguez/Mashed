400a9db039f2bbfebeb9fe73fe6bf96876e450a2
alanpnguyen123 <97857901+alanpnguyen123@users.noreply.github.com>
2023-04-12 19:22:51 -0400

    adding userpost and userpost php

---
+++ Mashed/adduserpost.php
@@ -0,0 +1,22 @@
+<?php
+
+include('db.php');
+
+$PostName = $_POST['PostName'];
+$PostBio = $_POST['PostBio'];
+$UserId = $_POST['UserId'];
+
+// Insert data into the database
+$sql = "INSERT INTO UserPost (PostName, PostBio, Poster, Likes, Shares, Comments, Bookmarks) VALUES ('$postName', '$postBio','$UserId', 0, 0, 0, 0)";
+
+if (mysqli_query($conn, $sql)) {
+    $response_array['status'] = 'success';
+    $response_array['Message'] = 'Post added successfully!';
+} else {
+    $response_array['status'] = 'error';
+    $response_array['Message'] = 'Error adding post: ' . mysqli_error($conn);
+}
+
+echo json_encode(array($response_array));
+$response[] = array("Message" => $Message);
+echo json_encode($response);
---
+++ Mashed/components/AddUserPost.js
@@ -0,0 +1,140 @@
+
+
+import React, {Component} from "react";
+import { View, Pressable, Text, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import { Image, Avatar, Button } from '@rneui/themed';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+export default class UserPost extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+        PostName: '',
+        PostBio: ''
+    };
+  }
+  
+  UserInfo=()=>{
+    var PostName = this.state.PostName;
+    var PostBio = this.state.PostBio;
+
+    var InsertAPIURL = "https://students.gaim.ucf.edu/~na404266/dig4104c/mashed-server/Addpost.php";   //API to render signup
+
+      var headers = {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      };
+      
+      var Data ={
+        PostName: PostName,
+        PostBio: PostBio
+      };
+
+    // FETCH func ------------------------------------
+    fetch(InsertAPIURL,{
+        method:'POST',
+        headers:headers,
+        body: JSON.stringify(Data) //convert data to JSON
+    })
+    .then((response)=>response.json()) //check response type of API (CHECK OUTPUT OF DATA IS IN JSON)
+    .then((response)=>{
+      alert(response[0].Message);       // If data is in JSON => Display alert msg
+      this.props.navigation.navigate("Home"); //Navigate to home if authentications are valid
+    })
+    .catch((error)=>{
+        alert("Error Occured" + error);
+    });
+    }
+  
+  render() {
+    return (
+      <>
+      <View style={styles.viewStyle}>
+        {/* LOGO */}
+          <Image  
+              style={{ height: '5%', width: '50%', marginHorizontal: '26%', marginTop: '10%', resizeMode:'contain'}}
+              source={{uri:'https://i.postimg.cc/65XBkHNg/logo.png'}}>  
+          </Image> 
+      </View>
+      <View style={[styles.container,{flexDirection: 'row'}]}>
+          <View style={{flex: 1}}>
+            <View style={styles.avatarSpacing}>
+                <Avatar
+                    size={120}
+                    rounded
+                    source={{ uri: "https://communication.ucf.edu/wp-content/uploads/sites/2/2018/05/Daniel-V.-Novatnak-1.jpeg" }}
+              />
+            </View>    
+          </View>
+        </View>
+      <View style={{ backgroundColor: 'white', borderRadius: '25 0 0 25', padding: '5%'}}>
+          <View style={styles.action}>
+            <TextInput
+              placeholder="Enter Recipe Name"
+              placeholderTextColor="#969696"
+              style={styles.textInput}
+              onChangeText={PostName=>this.setState({PostName})}
+              />
+          </View>
+      </View>
+      <View style={{ backgroundColor: 'white', borderRadius: '25 0 0 25', padding: '5%'}}>
+          <View style={styles.action}>
+            <TextInput
+              placeholder="Post Bio..."
+              placeholderTextColor="#969696"
+              style={styles.textInput}
+              onChangeText={PostBio=>this.setState({PostBio})}
+              />
+          </View>
+      </View>
+            {/* Button */}
+            <View style={styles.buttonsection}>    
+                <Pressable
+                  style={styles.button} 
+                  onPress={()=>{this.UserInfo()}}>
+                  <Text style={styles.text}>Share Post</Text>
+                </Pressable>
+              </View>
+
+
+
+      </>
+    )
+  }
+}
+const styles = StyleSheet.create({
+   
+  viewOne: {
+        textAlign: "center",
+        margin: 20,
+        marginBottom: 175,
+       
+    }, 
+    containerWhite:{
+      zIndex:2,
+      height:"60%",
+      width: "100%",
+      borderTopLeftRadius: 45,
+      borderTopRightRadius:45,
+      shadowColor: 'gray',
+      shadowOpacity: '20%',  
+      shadowOffset:{width: 0, height: -3},
+      backgroundColor: '#fff',    
+      alignItems: "center",
+      paddingTop:20, 
+    },
+    buttonsection: {
+      width: '100%',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    button: {
+      backgroundColor: '#FFC42D',
+      color: 'white',
+      height: 35,
+      justifyContent: 'center', //up dwn
+      alignItems: 'center',  //r & l
+      width: '70%',
+      borderRadius: 10,
+    }
+  })
