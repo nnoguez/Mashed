a45eedfa27f6388a5410327b6d45471e44cb6bbd
alanpnguyen123 <97857901+alanpnguyen123@users.noreply.github.com>
2023-04-13 22:57:57 -0400

    Update shoppinglist.js

--- Mashed/components/shoppinglist.js
+++ Mashed/components/shoppinglist.js
@@ -1,140 +1,45 @@
-import React, { useState, useEffect } from "react";
-import {
-  Keyboard,
-  View,
-  Image,
-  Text,
-  StyleSheet,
-  ScrollView,
-} from "react-native";
-import CartInput from "./CartInput";
-import CartItem from "./CartItem";
-import Icon from "react-native-vector-icons/FontAwesome";
-import Counter from "./counter";
-import { useRoute } from "@react-navigation/native";
+import React, {useState, useEffect} from "react";
+import {Keyboard, View, Image, Text, StyleSheet, ScrollView } from "react-native";
+import CartInput from './CartInput';
+import CartItem from './CartItem';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Counter from './counter';
+import { useRoute } from '@react-navigation/native';
 
-export default function Shopping({ navigation }) {
+
+export default function Shopping ({ navigation }) {  
   const [tasks, setTasks] = useState([]);
   const [recipes, setRecipes] = useState([]);
-  const route = useRoute();
-  const recipeId = route.params.recipeId;
-
   useEffect(() => {
-    fetch(
-      "https://students.gaim.ucf.edu/~na404266/dig4104c/mashed-server/Explore.php"
-    )
-      .then((response) => response.json())
-      .then((data) => setRecipes(data))
-      .catch((error) => console.error(error));
+    fetch('https://students.gaim.ucf.edu/~na404266/dig4104c/mashed-server/Explore.php')
+    .then(response => response.json())
+    .then(data => setRecipes(data))
+    .catch(error => console.error(error));
   }, []);
 
-  const addTask = (task) => {
-    if (task == null) return;
+  const route = useRoute(); // get the route object from the navigation stack
+  const { recipeId } = route.params; // get the recipeId parameter from the route
+
+  const recipe = recipes.find(r => r.id === recipeId); // find the recipe with the matching id
+
+  const addTask = (task) =>{
+    if (task==null) return;
     setTasks([...tasks, task]);
     Keyboard.dismiss();
-  };
+  }
 
   const deleteTask = (deleteIndex) => {
-    setTasks(tasks.filter((value, index) => index != deleteIndex));
-  };
+  setTasks(tasks.filter((value, index)=> index != deleteIndex));
+  }
+   
 
   return (
+        
     <View style={styles.backgroundWhite}>
-      <View
-        style={{
-          flexDirection: "row",
-          height: "15%",
-          backgroundColor: "white",
-          width: "100%",
-          alignItems: "center",
-          marginTop: 0,
-        }}
-      >
-        <View style={{ marginLeft: 20 }}>
-          <Icon
-            onPress={() =>
-              navigation.navigate("Recipe", { recipeId: recipeId })
-            }
-            name="angle-left"
-            size={35}
-            color="#FFC42D"
-          />
-        </View>
 
-        <Image
-          style={{
-            marginLeft: 100,
-            marginTop: 15,
-            width: "35%",
-            height: 25,
-          }}
-          source={{ uri: "https://i.postimg.cc/65XBkHNg/logo.png" }}
-        />
-      </View>
-      <View>
-        <Text style={styles.header}>SHOPPING LIST</Text>
-      </View>
-
-      {/* Adding ingredients to the shopping cart*/}
-      <ScrollView style={styles.scrollView}>
-        {tasks.map((task, index) => {
-          return (
-            <View key={index} style={styles.taskContainer}>
-              <CartItem
-                index={index + 1}
-                task={task}
-                deleteTask={() => deleteTask(index)}
-              />
-            </View>
-          );
-        })}
-      </ScrollView>
-      <CartInput addTask={addTask} />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  backgroundWhite: {
-    flex: 1,
-    backgroundColor: "white",
-  },
-
-  scrollView: {
-    marginBottom: 70,
-  },
-  taskContainer: {
-    marginTop: 20,
-  },
-  header: {
-    textAlign: "center",
-    fontSize: 15,
-    marginTop: 10,
-    color: "black",
-  },
-});
-
-  {/*    <Card borderRadius={25} width ={350} >
-          <View style={[styles.container,{flexDirection: 'row'}]}> 
-                <View style={{flex: 1, marginLeft: -20, marginRight: -60}}> 
-                  <CheckBox                     
-                    onPress={() => setChecked1([!checked1[0], checked1[1]])}>
-                    </CheckBox>                 
-                </View>      
-            <View style={{flex: 2}}>
-            <View style={{width:'100%'}}>
-                <Text style={styles.cardLabelStyle}>Chicken Breast{/* INser user Ingredient here}</Text>
-                <Counter/>
-            </View>
-            </View>
-          </View>
-        </Card> 
-
-
-
-<View style={{flexDirection:'row', height:'15%', backgroundColor:'white', width:'100%', alignItems:'center', marginTop:0}}>              
+      <View style={{flexDirection:'row', height:'15%', backgroundColor:'white', width:'100%', alignItems:'center', marginTop:0}}>              
               <View style={{marginLeft:20,}}>
-                <Icon onPress={()=> navigation.navigate('Recipe')}  name="angle-left" size={35} color="#FFC42D"/>
+                <Icon onPress={() => navigation.goBack()}  name="angle-left" size={35} color="#FFC42D"/>
               </View>
                  
                   <Image   
@@ -147,57 +52,47 @@ const styles = StyleSheet.create({
                   </Image>           
               
       </View>
+      <View>
+        <Text style={styles.header}>SHOPPING LIST</Text>
+      </View>
+       
+      {/* Adding ingredients to the shopping cart*/} 
+      <ScrollView style={styles.scrollView}>   
+        {
+          tasks.map((task, index) => {
 
+            return(
+              <View key={index} style={styles.taskContainer}>
+                <CartItem index={index+1} task={task} deleteTask={()=> deleteTask(index)}/>
+              </View>
 
-
-        totalStyle:{
-      fontSize: 20,
-      fontWeight: 'bold',
-      paddingRight:60,
-      color:'dark gray',
-      marginTop: 10
-    },
-    priceStyle:{
-      fontSize: 20,
-      fontWeight: 'bold',
-      color:"#9492ef",
-      paddingLeft:20,
-      paddingTop: 10,
-    },
-    headerStyle:{
-      zIndex:2,
-      position:'relative',    
-      marginTop:-750,
-      height:"20%",
-      width: "100%",
-      backgroundColor: '#fff',    
-      alignItems: "center", 
-      justifyContent: "center",
-      shadowColor:'gray',
-      shadowOffset: {width:0,height:-3},
-      shadowOpacity: '30%', 
-    },
-    cardStyle:{
+          );
+        })
+        }     
+                
+      </ScrollView>
+      <CartInput addTask={addTask}/>
+    </View>   
       
-      color:'white',
-      width: "80%",
-      height:100,
-      borderRadius:25,
+  );
+}
 
-    },
-    cardLabelStyle:{
-      fontSize: 15,
-      fontWeight: 'bold',
-      color:'dark gray',
-      paddingLeft:20,
-      marginLeft: -15,
-    },
-    cardWeightStyle:{
-      fontSize: 10,
-      fontWeight: 'bold',
-      color:'gray',
-      paddingLeft:20,
-      marginTop: 5
-    },
-      
-      */}
+const styles = StyleSheet.create({
+  backgroundWhite: {
+    flex:1,
+    backgroundColor: 'white',
+  },
+    
+  scrollView:{
+    marginBottom:70,
+  },
+  taskContainer:{
+    marginTop: 20,
+  },
+  header:{
+    textAlign:'center',
+    fontSize: 15,
+    marginTop:10,
+    color: 'black',
+  },
+});
