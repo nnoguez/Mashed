257153b7c3685052794a5bedfab5bc49632cea85
alanpnguyen123 <97857901+alanpnguyen123@users.noreply.github.com>
2023-04-13 22:54:31 -0400

    Update shoppinglist.js

--- Mashed/components/shoppinglist.js
+++ Mashed/components/shoppinglist.js
@@ -1,98 +1,118 @@
-import React, {useState, useEffect} from "react";
-import {Keyboard, View, Image, Text, StyleSheet, ScrollView } from "react-native";
-import CartInput from './CartInput';
-import CartItem from './CartItem';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Counter from './counter';
-import { useRoute } from '@react-navigation/native';
+import React, { useState, useEffect } from "react";
+import {
+  Keyboard,
+  View,
+  Image,
+  Text,
+  StyleSheet,
+  ScrollView,
+} from "react-native";
+import CartInput from "./CartInput";
+import CartItem from "./CartItem";
+import Icon from "react-native-vector-icons/FontAwesome";
+import Counter from "./counter";
+import { useRoute } from "@react-navigation/native";
 
-
-export default function Shopping ({ navigation }) {  
+export default function Shopping({ navigation }) {
   const [tasks, setTasks] = useState([]);
   const [recipes, setRecipes] = useState([]);
+  const route = useRoute();
+  const recipeId = route.params.recipeId;
+
   useEffect(() => {
-    fetch('https://students.gaim.ucf.edu/~na404266/dig4104c/mashed-server/Explore.php')
-    .then(response => response.json())
-    .then(data => setRecipes(data))
-    .catch(error => console.error(error));
-}, []);
-  const addTask = (task) =>{
-    if (task==null) return;
+    fetch(
+      "https://students.gaim.ucf.edu/~na404266/dig4104c/mashed-server/Explore.php"
+    )
+      .then((response) => response.json())
+      .then((data) => setRecipes(data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  const addTask = (task) => {
+    if (task == null) return;
     setTasks([...tasks, task]);
     Keyboard.dismiss();
-  }
+  };
 
   const deleteTask = (deleteIndex) => {
-  setTasks(tasks.filter((value, index)=> index != deleteIndex));
-  }
-   
+    setTasks(tasks.filter((value, index) => index != deleteIndex));
+  };
 
   return (
-        
-  <View style={styles.backgroundWhite}>
+    <View style={styles.backgroundWhite}>
+      <View
+        style={{
+          flexDirection: "row",
+          height: "15%",
+          backgroundColor: "white",
+          width: "100%",
+          alignItems: "center",
+          marginTop: 0,
+        }}
+      >
+        <View style={{ marginLeft: 20 }}>
+          <Icon
+            onPress={() =>
+              navigation.navigate("Recipe", { recipeId: recipeId })
+            }
+            name="angle-left"
+            size={35}
+            color="#FFC42D"
+          />
+        </View>
 
-      <View style={{flexDirection:'row', height:'15%', backgroundColor:'white', width:'100%', alignItems:'center', marginTop:0}}>              
-              <View style={{marginLeft:20,}}>
-                <Icon onPress={() => navigation.navigate('Recipe', { recipeId: recipe.id })}  name="angle-left" size={35} color="#FFC42D"/>
-              </View>
-                 
-                  <Image   
-                        style={{
-                          marginLeft: 100, 
-                          marginTop: 15,
-                          width:"35%", 
-                          height:25}}
-                        source={{uri:'https://i.postimg.cc/65XBkHNg/logo.png'}}>  
-                  </Image>           
-              
+        <Image
+          style={{
+            marginLeft: 100,
+            marginTop: 15,
+            width: "35%",
+            height: 25,
+          }}
+          source={{ uri: "https://i.postimg.cc/65XBkHNg/logo.png" }}
+        />
       </View>
       <View>
         <Text style={styles.header}>SHOPPING LIST</Text>
       </View>
-       
-      {/* Adding ingredients to the shopping cart*/} 
-      <ScrollView style={styles.scrollView}>   
-        {
-          tasks.map((task, index) => {
 
-            return(
-              <View key={index} style={styles.taskContainer}>
-                <CartItem index={index+1} task={task} deleteTask={()=> deleteTask(index)}/>
-              </View>
-
+      {/* Adding ingredients to the shopping cart*/}
+      <ScrollView style={styles.scrollView}>
+        {tasks.map((task, index) => {
+          return (
+            <View key={index} style={styles.taskContainer}>
+              <CartItem
+                index={index + 1}
+                task={task}
+                deleteTask={() => deleteTask(index)}
+              />
+            </View>
           );
-        })
-        }     
-                
+        })}
       </ScrollView>
-      <CartInput addTask={addTask}/>
-</View>   
-      
-    );
-  }
+      <CartInput addTask={addTask} />
+    </View>
+  );
+}
 
-  const styles = StyleSheet.create({
-
-
-    backgroundWhite: {
-     flex:1,
-     backgroundColor: 'white',
-    },
-    
-   scrollView:{
-    marginBottom:70,
-   },
-   taskContainer:{
-    marginTop: 20,
-   },
-  header:{
-    textAlign:'center',
-    fontSize: 15,
-    marginTop:10,
-    color: 'black',
+const styles = StyleSheet.create({
+  backgroundWhite: {
+    flex: 1,
+    backgroundColor: "white",
   },
 
-  })
+  scrollView: {
+    marginBottom: 70,
+  },
+  taskContainer: {
+    marginTop: 20,
+  },
+  header: {
+    textAlign: "center",
+    fontSize: 15,
+    marginTop: 10,
+    color: "black",
+  },
+});
 
   {/*    <Card borderRadius={25} width ={350} >
           <View style={[styles.container,{flexDirection: 'row'}]}> 
