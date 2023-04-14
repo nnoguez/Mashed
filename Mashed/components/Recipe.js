c5fae1140f1f6377ee2d54cda0e7d2079f6fd31f
naomy <92695534+nnoguez@users.noreply.github.com>
2023-04-14 17:16:13 -0400

    functionality recipe

--- Mashed/components/Recipe.js
+++ Mashed/components/Recipe.js
@@ -6,182 +6,75 @@ import { AirbnbRating } from "@rneui/base";
 import { useRoute } from '@react-navigation/native';
 
 export default function Recipe({ navigation }) {
-    const [checked1, setChecked1] = React.useState([false, false]);
-    const [expanded, setExpanded] = useState(false);
-    const [expanded2, setExpanded2] = useState(false);
-    const [recipes, setRecipes] = useState([]);
-    // const route = useRoute();
-    // const { recipeId } = route.params;
+    const [recipe, setRecipe] = useState([]);
+    const route = useRoute();
+    const { recipeId } = route.params;
+
     useEffect(() => {
-        fetch(`https://students.gaim.ucf.edu/~na404266/dig4104c/mashed-server/Recipe.php`)
-          .then((response) => response.json())
-          .then((data) => setRecipes(data))
-          .catch((error) => console.error(error));
-      });
-    return (
-    <>
-    {/* mashed logo */}
-   {/* mashed logo */}
-   <View style={styles.viewOne}>
-        <View>
-            <View style={[styles.container,{flexDirection: 'row'}]}>
-            <TouchableOpacity onPress={()=> navigation.navigate('Home')}>
-                <Icon style={{ marginRight: 355, marginTop: 20 }} name="angle-left" size={35} color="#FFC42D" />
-            </TouchableOpacity>
-            <TouchableOpacity onPress={()=> navigation.navigate('Shopping', { recipeId: recipe.id })}>
-                <Icon style={{ marginLeft: -15, marginTop: 20 }} name="shopping-basket" size={25} color="#FFC42D"/>
-            </TouchableOpacity>
-        </View>
-            <Image
-            style={{ 
-                marginLeft: 127, 
-                marginBottom: 10,
-                width:"35%", 
-                height:25}}
-            source={{
-            uri:'https://i.postimg.cc/65XBkHNg/logo.png',
-            }}
-            /> 
-    </View>
-</View>
-    <ScrollView>
-
-    {/* image with heart icon */}
-        <Image style={{width:"100%", height:300, marginTop: 5, marginTop: 5, borderBottomRightRadius: 25, borderBottomLeftRadius: 25}}
-            source={{
-            uri:'https://www.thespruceeats.com/thmb/a8cS7kg5bbsuFsJN-5zO3eOVvBE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/chicken-katsu-4778466-10-67e6122e936b418ab1a92f176709d299.jpg',
-            }}>
-        </Image>
-        {recipes.map((recipe)=> (
-        <View style={{ margin:20 }}>
-        {/* Title and star rating */}
-            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'flex-start'}}>
-                <View style={[{flexDirection: 'row', marginLeft: -6 }]}>
-                    <Text style={styles.header}> {recipe.RecipeName}</Text>
-                </View>
-                <View style={{ flexDirection: 'row' }}>
-                    <View style={{justifyContent:'space-evenly', marginLeft: 62}}>
-                        <AirbnbRating size={20} showRating={false} isDisabled={true} defaultRating={4}/>
-                    </View>
-                </View>
-            </View>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginTop: 10 }}>
-                <Text style={styles.Subheader}>By: sakuraiiko</Text>
-                <Text style={styles.Subheader}>618 Reviews</Text>
-            </View>
-
-            <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', paddingBottom: 10, borderBottomWidth: 1, borderBottomColor: 'lightgrey'}}>
-                <View style={[{flexDirection: 'row', marginTop: 10 }]}>
-                    <Text style={styles.body}>{recipe.RecipeDescription}</Text>
-                </View>
-            </View>
-
-            <View style={{ flexDirection: 'row',justifyContent: 'space-evenly', marginTop: 10 }}>
-                <Text style={styles.Subheader}> {recipe.PrepTime} mins </Text>
-                <Text style={styles.Subheader}> {recipe.CookTime} mins </Text>
-            </View>
-            <View style={{ flexDirection: 'row',justifyContent: 'space-evenly', marginTop: 10, fontWeight: 'bold' }}>
-                <Text style={styles.Subheader}> {recipe.Difficulty} </Text>
-                <Text style={styles.Subheader}> {recipe.ServingSize} people </Text>
-            </View>
-
-        <View style={{paddingBottom: 10, borderBottomWidth: 1, borderBottomColor: 'lightgrey', marginBottom: 10}}/>
-
-            <Text style={{ fontSize: 19, color: '#242523', fontWeight: 'bold', marginLeft: 15 }}>Dietary Restrictions</Text>
-            
-            <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 10, marginLeft: 3, marginRight: 50 }}>
-                <Text style={styles.Subheader}>
-                Halal, Kosher
-                </Text>
-                <Text style={styles.Subheader}>
-                Contains Egg, Dairy
-                </Text>
-            </View>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-around', marginTop: 10, marginLeft: 3, marginRight: 50 }}> 
-                <Text style={styles.Subheader}>
-                Nut-free
-                </Text>
-                <Text style={styles.Subheader}>
-                Lactose-Free
-                </Text>
-            </View>
-
-        <View style={{paddingBottom: 10, borderBottomWidth: 1, borderBottomColor: 'lightgrey', marginBottom: 10, marginTop: 10}}/>
-
-        <ListItem.Accordion containerStyle={{
-            borderRadius: 20,
-            backgroundColor: '#F2F2F2',
-        }}
-            content={
-            <ListItem.Content>
-                <ListItem.Subtitle style={styles.header}> 
-                    Ingredients
-                </ListItem.Subtitle>
-            </ListItem.Content>
+        const fetchData = async () => {
+          try {
+            const response = await fetch(`https://students.gaim.ucf.edu/~na404266/dig4104c/mashed-server/Recipe.php?RecipeId=${recipeId}`);
+            const data = await response.json();
+            if (data && data.length > 0) {
+              setRecipe(data[0]);
             }
-            isExpanded={expanded}
-            onPress={() => {
-            setExpanded(!expanded);
-            }}>
-            
-        {/* ingredient 1 */}
-            <ListItem containerStyle={{
-                borderRadius: 20,
-                backgroundColor: '#F2F2F2'
-            }}>
-                <ListItem.CheckBox
-                    iconType="material-community"
-                    checkedIcon="checkbox-marked"
-                    uncheckedIcon="checkbox-blank-outline"
-                    checked={checked1[0]}
-                    onPress={() => setChecked1([!checked1[0], checked1[1]])}
-                    containerStyle= {{
-                        backgroundColor:'#F2F2F2'
-                    }}
-                    checkedColor='#FFC42D'
-                /> 
-                <ListItem.Content>
-                    <ListItem.Title>{recipe.RecipeIngredients}</ListItem.Title>
-                </ListItem.Content>
-            </ListItem>
-        </ListItem.Accordion>
-
-        <View style={{paddingBottom: 10, borderBottomWidth: 1, borderBottomColor: 'lightgrey', marginBottom: 10, marginTop: 10}}/>
-
-        <ListItem.Accordion containerStyle={{
-            borderRadius: 20,
-            backgroundColor: '#F2F2F2',
-        }}
-            content={
-            <ListItem.Content>
-                <ListItem.Subtitle style={styles.header}> 
-                    Directions
-                </ListItem.Subtitle>
-            </ListItem.Content>
-            }
-            isExpanded={expanded2}
-            onPress={() => {
-            setExpanded2(!expanded2);
-            }}>
-            
-            {/* ingredient 1 */}
-            <ListItem containerStyle={{
-                borderRadius: 20,
-                backgroundColor: '#F2F2F2'
-            }}>
-                <ListItem.Content>
-                    <ListItem.Title>{recipe.RecipeSteps} </ListItem.Title>
-                </ListItem.Content>
-            </ListItem>
-        
-        </ListItem.Accordion>
-      
+          } catch (error) {
+            console.error(error);
+          }
+        };
+        fetchData();
+      }, [recipeId]);
+    
+      return (
+        <>
+          {/* mashed logo */}
+          <View style={styles.viewOne}>
+            <View>
+              <View style={[styles.container, { flexDirection: 'row' }]}>
+                <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+                  <Icon style={{ marginRight: 355, marginTop: 20 }} name="angle-left" size={35} color="#FFC42D" />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => navigation.navigate('Shopping', { recipeId: recipeId })}>
+                  <Icon style={{ marginLeft: -15, marginTop: 20 }} name="shopping-basket" size={25} color="#FFC42D" />
+                </TouchableOpacity>
+              </View>
+              <Image
+                style={{
+                  marginLeft: 127,
+                  marginBottom: 10,
+                  width: "35%",
+                  height: 25
+                }}
+                source={{
+                  uri: 'https://i.postimg.cc/65XBkHNg/logo.png',
+                }}
+              />
             </View>
-              ))}
-    </ScrollView>
+            {/* Recipe details */}
+            <View style={styles.viewTwo}>
+              <ScrollView>
+                <View style={styles.recipeContainer}>
+                  <Text style={styles.recipeName}>{recipe.RecipeName}</Text>
+                  <Text style={styles.recipeDescription}>{recipe.RecipeDescription}</Text>
+                  <Text style={styles.recipeHeading}>Ingredients:</Text>
+                  <Text style={styles.recipeDetails}>{recipe.RecipeIngredients}</Text>
+                  <Text style={styles.recipeHeading}>Steps:</Text>
+                  <Text style={styles.recipeDetails}>{recipe.RecipeStep}</Text>
+                  <Text style={styles.recipeHeading}>Prep Time:</Text>
+                  <Text style={styles.recipeDetails}>{recipe.PrepTime} minutes</Text>
+                  <Text style={styles.recipeHeading}>Cook Time:</Text>
+                  <Text style={styles.recipeDetails}>{recipe.CookTime} minutes</Text>
+                  <Text style={styles.recipeHeading}>Difficulty:</Text>
+                  <Text style={styles.recipeDetails}>{recipe.Difficulty}</Text>
+                  <Text style={styles.recipeHeading}>Serving Size:</Text>
+                  <Text style={styles.recipeDetails}>{recipe.ServingSize}</Text>
+                </View>
+              </ScrollView>
+            </View>
+          </View>
         </>
-    )
-}
+      );
+    }
 
 const styles = StyleSheet.create({
     viewOne: {
