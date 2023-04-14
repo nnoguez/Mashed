18d6b59c383a5a18534b513ce631f1d8272ce9c3
alanpnguyen123 <97857901+alanpnguyen123@users.noreply.github.com>
2023-04-14 17:26:00 -0400

    Update Recipe.php

--- Mashed/Recipe.php
+++ Mashed/Recipe.php
@@ -1,18 +1,20 @@
 <?php
 include('db.php');
 
-// Get the recipe ID from the URL parameter
-// $recipe_id = $_GET['recipe_id'];
+if (isset($_GET['RecipeId'])) {
+    $recipeId = $_GET['RecipeId'];
+    $sql = "SELECT * FROM Recipes WHERE RecipeId = $recipeId";
+    $result = mysqli_query($conn, $sql);
 
-$sql = "SELECT * FROM Recipes WHERE RecipeId = 1";
-$result = mysqli_query($conn, $sql);
-
-if (mysqli_num_rows($result) > 0) {
-    // Store the result in an associative array
-    $recipes = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    // Return the array as a JSON response
-    echo json_encode($recipes);
+    if (mysqli_num_rows($result) > 0) {
+        // Store the result in an associative array
+        $recipes = mysqli_fetch_all($result, MYSQLI_ASSOC);
+        // Return the array as a JSON response
+        echo json_encode($recipes);
+    } else {
+        echo "No posts yet";
+    }
 } else {
-    echo "No posts yet";
+    echo "Recipe ID not specified";
 }
 ?>
